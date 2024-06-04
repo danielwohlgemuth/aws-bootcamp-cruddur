bd18f8a18ce68190d24236868668ce2e35c6b81c
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2024-06-03 20:05:18 -0400

    Fix database scripts

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -4,21 +4,21 @@ DROP TABLE IF EXISTS public.activities;
 
 CREATE TABLE public.users (
     uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    display_name text NOT NULL,
-    handle text NOT NULL,
-    email text NOT NULL,
-    cognito_user_id text NOT NULL,
-    created_at TIMESTAMP default current_timestamp NOT NULL
+    display_name TEXT NOT NULL,
+    handle TEXT NOT NULL,
+    email TEXT NOT NULL,
+    cognito_user_id TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
 );
 
 CREATE TABLE public.activities (
     uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    user_id UUID NOT NULL,
-    message text NOT NULL,
+    user_uuid UUID NOT NULL,
+    message TEXT NOT NULL,
     replies_count INTEGER DEFAULT 0,
     reposts_count INTEGER DEFAULT 0,
     likes_count INTEGER DEFAULT 0,
     reply_to_activity_uuid INTEGER,
     expires_at TIMESTAMP,
-    created_at TIMESTAMP DEFAULT curren NOT NULL
+    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
 );
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,11 +1,12 @@
 INSERT INTO public.users (
     display_name,
     handle,
+    email,
     cognito_user_id
 )
 VALUES
-    ('Andrew Brown', 'andrewbrown', 'MOCK'),
-    ('Andrew Bayko', 'andrewbayko', 'MOCK')
+    ('Andrew Brown', 'andrewbrown', 'andrewbrown@example.com.invalid', 'MOCK'),
+    ('Andrew Bayko', 'andrewbayko', 'andrewbayko@example.com.invalid', 'MOCK');
 
 INSERT INTO public.activities (
     user_uuid,
@@ -22,4 +23,4 @@ VALUES
         ),
         'This was imported as seed data!',
         current_timestamp + interval '10 day'
-    )
+    );
